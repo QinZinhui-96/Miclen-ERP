58364609372ebb266c5c6669f943c01d3ba88f2f
zhqinw <zhqinw>
2026-06-27 11:47:22 +0800

    修改

--- miclen_base/miclen_project/models/miclen_category_details.py
+++ miclen_base/miclen_project/models/miclen_category_details.py
@@ -8,7 +8,7 @@ _logger = logging.getLogger(__name__)
 
 class MiclenProductCategoryDetails(models.Model):
     _name = 'miclen.category.details'
-    _description = "miclen产品中类"
+    _description = "miclen产品中类 "
 
     sequence = fields.Integer('序号', default=10)
     name = fields.Char('编码', tracking=True)
