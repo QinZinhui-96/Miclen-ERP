56caecd393116edfc7686ad8976b8fd5324b8bbe
qinzhihui <907839210@qq.com>
2026-07-22 15:53:44 +0800

    添加空格

--- miclen_base/access_roles/models/access_role.py
+++ miclen_base/access_roles/models/access_role.py
@@ -9,7 +9,7 @@
 #    You can modify it under the terms of the GNU AFFERO
 #    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
 #
-#    This program is distributed in the hope that it will be useful,
+#    This program is distributed in the hope that it will be useful, 
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
